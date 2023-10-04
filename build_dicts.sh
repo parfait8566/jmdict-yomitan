01fa9789920b8ee691e48b9fb6dd8e71068c87d7
parfait8566 <146892521+parfait8566@users.noreply.github.com>
2023-10-04 08:50:22 +0200

    Update build_dicts.sh

--- build_dicts.sh
+++ build_dicts.sh
@@ -5,6 +5,8 @@
 mkdir -p src
 mkdir -p dst
 
+current-date=$(date '+%Y-%m-%d')
+
 function refresh_source () {
     NOW=$(date '+%s')
     YESTERDAY=$((NOW - 86400)) # 86,400 seconds in 24 hours
@@ -17,27 +19,12 @@ function refresh_source () {
 }
 
 refresh_source "JMdict_e_examp"
-./binaries/yomitan -language="english_extra" -title="JMdict" src/JMdict_e_examp dst/JMdict_english_with_examples.zip
+./binaries/yomitan -language="english_extra" -title="JMdict" src/JMdict_e_examp dst/"$current-date"_JMdict_english_with_examples.zip
 
-refresh_source "JMdict"
-./binaries/yomitan -language="english_extra" -title="JMdict"         src/JMdict dst/JMdict_english.zip
-./binaries/yomitan -language="english"   -title="JMdict (Legacy)"    src/JMdict dst/JMdict_english_legacy.zip
-./binaries/yomitan -language="dutch"     -title="JMdict (Dutch)"     src/JMdict dst/JMdict_dutch.zip
-./binaries/yomitan -language="french"    -title="JMdict (French)"    src/JMdict dst/JMdict_french.zip
-./binaries/yomitan -language="german"    -title="JMdict (German)"    src/JMdict dst/JMdict_german.zip
-./binaries/yomitan -language="hungarian" -title="JMdict (Hungarian)" src/JMdict dst/JMdict_hungarian.zip
-./binaries/yomitan -language="russian"   -title="JMdict (Russian)"   src/JMdict dst/JMdict_russian.zip
-./binaries/yomitan -language="slovenian" -title="JMdict (Slovenian)" src/JMdict dst/JMdict_slovenian.zip
-./binaries/yomitan -language="spanish"   -title="JMdict (Spanish)"   src/JMdict dst/JMdict_spanish.zip
-./binaries/yomitan -language="swedish"   -title="JMdict (Swedish)"   src/JMdict dst/JMdict_swedish.zip
-
-./binaries/yomitan -format="forms"       -title="JMdict Forms"       src/JMdict dst/JMdict_forms.zip
+./binaries/yomitan -format="forms"       -title="JMdict Forms"       src/JMdict dst/"$current-date"_JMdict_forms.zip
 
 refresh_source "JMnedict.xml"
-./binaries/yomitan src/JMnedict.xml dst/JMnedict.zip
+./binaries/yomitan src/JMnedict.xml dst/"$current-date"_JMnedict.zip
 
 refresh_source "kanjidic2.xml"
-./binaries/yomitan -language="english"    -title="KANJIDIC"              src/kanjidic2.xml dst/KANJIDIC_english.zip
-./binaries/yomitan -language="french"     -title="KANJIDIC (French)"     src/kanjidic2.xml dst/KANJIDIC_french.zip
-./binaries/yomitan -language="portuguese" -title="KANJIDIC (Portuguese)" src/kanjidic2.xml dst/KANJIDIC_portuguese.zip
-./binaries/yomitan -language="spanish"    -title="KANJIDIC (Spanish)"    src/kanjidic2.xml dst/KANJIDIC_spanish.zip
+./binaries/yomitan -language="english"    -title="KANJIDIC"              src/kanjidic2.xml dst/"$current-date"_KANJIDIC_english.zip
